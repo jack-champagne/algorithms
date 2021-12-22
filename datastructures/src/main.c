5472a0e692b4fc65681f23b08ad27382cf016681
Jack Champagne <jackchampagne.r@gmail.com>
2021-12-21 23:50:30 -0500

    Add linked list implementation

--- datastructures/src/main.c
+++ datastructures/src/main.c
@@ -1,6 +1,7 @@
 #include "vec.h"
 
 int main() {
+    // Code examples?
     LinkedList* new_llist = linked_list();
     int* dyn_int = (int*) malloc(sizeof(int));
     *dyn_int = 255;
@@ -8,12 +9,23 @@ int main() {
     int* dyn_int2 = (int*) malloc(sizeof(int));
     *dyn_int2 = 871;
 
+    int i = 552;
+
     ll_append(new_llist, dyn_int);
-    ll_append(new_llist, dyn_int2);
-    ll_append(new_llist, dyn_int2);
+    ll_prepend(new_llist, dyn_int2);
+    ll_append(new_llist, &i);
     printf("%d: %d, %d, %d\n", ll_is_empty(new_llist), *(int*)(new_llist->head->data), *(int*)(new_llist->head->next->data), *(int*)(new_llist->head->next->next->data));
-    *dyn_int2 = 65;
-    printf("%d: %d, %d, %d\n", ll_is_empty(new_llist), *(int*)(new_llist->head->data), *(int*)(new_llist->head->next->data), *(int*)(new_llist->head->next->next->data));
+    printf("%d\n", new_llist->length);
+    printf("%d\n", *(int*)new_llist->tail->data);
+    ll_prepend(new_llist, &i);
+    printf("%d: %d, %d, %d, %d\n", ll_is_empty(new_llist), *(int*)(new_llist->head->data), *(int*)(new_llist->head->next->data), *(int*)(new_llist->head->next->next->data), *(int*)(new_llist->tail->data));
+    printf("%d\n", *(int*)ll_get(new_llist, 0));
+    ll_prepend(new_llist, dyn_int2);
+    printf("%d: %d, %d, %d, %d, %d\n", ll_is_empty(new_llist), *(int*)(new_llist->head->data), *(int*)(new_llist->head->next->data), *(int*)(new_llist->head->next->next->data), *(int*)(new_llist->head->next->next->next->data), *(int*)(new_llist->tail->data));
+    ll_remove(new_llist, 0);
+    printf("%d: %d, %d, %d, %d\n", ll_is_empty(new_llist), *(int*)(new_llist->head->data), *(int*)(new_llist->head->next->data), *(int*)(new_llist->head->next->next->data), *(int*)(new_llist->head->next->next->next->data));
+    printf("%d\n", *(int*)ll_get(new_llist, 0));
+    printf("%d\n", *(int*)ll_get(new_llist, 3));
     free(dyn_int);
     free(dyn_int2);
     ll_destroy(new_llist);
--- datastructures/src/vec.c
+++ datastructures/src/vec.c
@@ -1,15 +1,24 @@
 #include "vec.h"
 
+/// Mallocs a new linked list struct and initializes fields.
+/// Usage:
+/// LinkedList* my_list = linked_list();
+/// 
+/// Returns pointer to dynamically allocated linked list
 LinkedList* linked_list() {
     LinkedList* ret = (LinkedList*) malloc(sizeof(LinkedList));
     ret->length = 0;
     ret->head = NULL;
+    ret->tail = NULL;
+
+    return ret;
 }
 
 /// Deallocs the list, all nodes contained, and data contained as well
 /// Usage:
 /// ll_destroy(my_list);
-/// Returns 
+///
+/// Returns void
 void ll_destroy(LinkedList* list) {
     while (list->head != NULL) {
         // free(cur->data); //TODO: This line may cause problematic double frees on the same data.
@@ -24,32 +33,126 @@ void ll_destroy(LinkedList* list) {
 /// for the new node. This is necessary because we cannot know for sure if
 /// the library user will malloc and free properly.
 /// Usage example:
-/// append(my_list, data_to_add);
-/// Implementation details:
-void ll_append(LinkedList* list, void* data_to_add) {
-    //TODO: Add tail field in LinkedList struct for O(1) insertion
-    if (list == NULL || data_to_add == NULL) {
-        fprintf(stderr, "append list pointer null");
-    }
-    
-    Node* cur = list->head;
-    // No elements yet in list
-    if (cur == NULL) {
-        list->head = (Node*) malloc(sizeof(Node));
-        list->head->data = data_to_add;
-        list->head->next = NULL;
-        list->length++;
-    } else {
-        while (cur->next != NULL) {
-            cur = cur->next;
-        }
-        cur->next = (Node*) malloc(sizeof(Node));
-        cur->next->data = data_to_add;
-        cur->next->next = NULL;
-        list->length++;
-    }
+/// append(my_list, data_to_append);
+/// 
+/// Returns void
+void ll_append(LinkedList* list, void* data_to_append) {
+    ll_insert_after(list, list->tail, data_to_append);
 }
 
+/// Inserts node after the node given and manages pointers properly. Assume node
+/// to insert is a new node that is not currently in the list.
+/// Special case: if prev is NULL, insert node at head.
+/// Usage:
+/// ll_insert_after(my_list, predecessor_node, new_node);
+///
+/// Returns void
+void ll_insert_after(LinkedList* list, Node* prev, void* data_to_insert) {
+    if (list == NULL || data_to_insert == NULL) {
+        fprintf(stderr, "list or data_to_insert NULL, cannot insert after");
+        return;
+    }
+
+    // Design choice: Originally, this implementation for insert_after did not
+    // include a LinkedList* list parameter because insertion of a node only requires
+    // the proper swapping of references. This was changed to allow the list->length
+    // parameter to remain updated.
+    Node* new_node = ll_node(data_to_insert);
+    if (list->head == NULL) {
+        list->head = new_node;
+        list->tail = new_node;
+        list->length = 1;
+        return;
+    }
+
+    Node* successor;
+    if (prev == NULL) {
+        successor = list->head;
+        list->head = new_node;
+
+    } else {
+        if (prev == list->tail) {
+            list->tail = new_node;
+        }
+        successor = prev->next;
+        prev->next = new_node;
+    }
+
+    new_node->next = successor;
+    list->length++;
+}
+
+/// Prepend data to beginning of list (the head)
+/// Usage:
+/// ll_prepend(my_list, data_to_prepend)
+void ll_prepend(LinkedList* list, void* data_to_prepend) {
+    ll_insert_after(list, NULL, data_to_prepend);
+}
+
+
+/// Returns the data at specified index in linked list
+/// Usage:
+/// printf("%d", *(int*)ll_get(my_list, 1)); // Prints second element in my_list if it exists.
+/// Runtime: O(n)
+void* ll_get(LinkedList* list, int index) {
+    if (list == NULL || index < 0 || index >= list->length) {
+        fprintf(stderr, "list null or index out of bounds");
+        return NULL;
+    }
+
+    return get_node(list, index)->data;
+}
+
+/// 
+void* ll_remove(LinkedList* list, int index) {
+    if (list == NULL || index < 0 || index >= list->length) {
+        fprintf(stderr, "list null or index out of bounds");
+        return NULL;
+    }
+
+    Node* removed_node, * prev;
+    if (index == 0) {
+        // Change head
+        removed_node = list->head;
+        list->head = removed_node->next;
+        prev = NULL;
+    } else {
+        // Repair list
+        Node* prev = get_node(list, index - 1);
+        removed_node = get_node(list, index);
+        prev->next = removed_node->next;
+    }
+    // Fix tail pointer
+    if (removed_node == list->tail) {
+        list->tail = prev;
+    }
+    list->length--;
+    
+    void* ret = removed_node->data;
+    free(removed_node);
+    return ret;
+}
+
+/// Get node at index. Not for outside use. For library use only.
+/// Usage:
+/// Node* node_at_index = ll_get(my_list, 1); // Returns the second node in the list if it exists
+/// Returns Node* to node struct rep. index i.
+Node* get_node(LinkedList* list, int index) {
+    Node* ret = list->head;
+    int i = 0;
+    while (ret != NULL && i < index) {
+        ret = ret->next;
+        i++;
+    }
+
+    return ret;
+}
+
+/// Check to see if list is empty
+/// Usage:
+/// if (ll_is_empty(my_list)) { foo(); }
+///
+/// Returns 1 if empty, 0 if not empty.
 int ll_is_empty(LinkedList* list) {
     if (list == NULL) {
         fprintf(stderr, "list pointer null");
@@ -57,3 +160,9 @@ int ll_is_empty(LinkedList* list) {
     return (list->length == 0);
 }
 
+Node* ll_node(void* new_data) {
+    Node* ret = (Node*) malloc(sizeof(Node));
+    ret->data = new_data;
+    ret->next = NULL;
+    return ret;
+}
